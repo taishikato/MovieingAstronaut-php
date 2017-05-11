a082655580c7a6d84f108e8d57a0589c5400f9d9
takshikato <taishi.k0903@gmail.com>
2017-05-11 21:38:18 +0900

    5/11_.htaccess

---
+++ Utility/config.js
@@ -0,0 +1,3 @@
+redeyesmovieConfig.signupWithFacebookUrl  = 'http://redeyesmovie.life/users/signupWithFacebook';
+redeyesmovieConfig.loginWithFacebookUrl   = 'http://redeyesmovie.life/users/loginWithFacebook';
+redeyesmovieConfig.connectWithFacebookUrl = 'http://redeyesmovie.life/authentications/facebook';
---
+++ Utility/deploy.sh
@@ -0,0 +1,8 @@
+echo 'database.php設置'
+mv -v ./database.php ../Config/
+
+echo 'config.js設置'
+mv -v ./config.js ../webroot/js/
+
+echo 'htaccess設置'
+mv -v ./htaccess ../.htaccess
