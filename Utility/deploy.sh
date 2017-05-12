6137ab93f99718524f0822ef8ebacb201cf14feb
takshikato <taishi.k0903@gmail.com>
2017-05-12 19:49:32 +0900

    5/12_htaccess系修正

--- Utility/deploy.sh
+++ Utility/deploy.sh
@@ -5,4 +5,6 @@ echo 'config.js設置'
 mv -v ./config.js ../webroot/js/
 
 echo 'htaccess設置'
-mv -v ./htaccess ../.htaccess
+mv -v ./htaccess ../../.htaccess
+mv -v ./app_htaccess ../.htaccess
+mv -v ./webroot_htaccess ../webroot/..htaccess
